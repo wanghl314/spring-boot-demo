7ecf602182fc330c1cf15f29e82a11ca39ebead6
王洪亮 <w505957125@aliyun.com>
2023-12-06 16:18:26 +0800

    1. add org.redisson:redisson-spring-boot-starter 2. switch to spring.data.mongodb.uri 3. add RedissonLock Storage demo

---
+++ src/main/java/com/whl/spring/demo/config/StorageConfig.java
@@ -0,0 +1,67 @@
+package com.whl.spring.demo.config;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+@Configuration
+@EnableScheduling
+public class StorageConfig {
+    public static final String STORAGE_LOCK = "storage-lock";
+
+    private static final int DEFAULT_AMOUNT = 100;
+
+    private static Logger logger = LoggerFactory.getLogger(StorageConfig.class);
+
+    private int storage = DEFAULT_AMOUNT;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    public int getStorage() {
+        return storage;
+    }
+
+    public int reduceStorage() {
+        return this.reduceStorage(1);
+    }
+
+    public int reduceStorage(int count) {
+        if (this.storage <= 0) {
+            return 0;
+        }
+        int realCount = Math.min(this.storage, count);
+        this.storage -= count;
+
+        if (this.storage < 0) {
+            this.storage = 0;
+        }
+        logger.info("消耗了 {} 个库存，还剩 {} 个库存。", realCount, this.storage);
+        return realCount;
+    }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void refreshRoutes() {
+        RLock lock = this.redissonClient.getLock(STORAGE_LOCK);
+
+        try {
+            lock.lock();
+            final int COUNT = DEFAULT_AMOUNT - Math.max(this.storage, 0);
+
+            if (COUNT > 0) {
+                this.storage = DEFAULT_AMOUNT;
+                logger.info("补充 {} 个库存，总共 {} 个库存。", COUNT, this.storage);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
--- src/main/java/com/whl/spring/demo/controller/RedisController.java
+++ src/main/java/com/whl/spring/demo/controller/RedisController.java
@@ -1,5 +1,11 @@
 package com.whl.spring.demo.controller;
 
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,13 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.whl.spring.demo.config.StorageConfig;
 
 @RestController
 @RequestMapping("/redis")
 public class RedisController {
+    private static Logger logger = LoggerFactory.getLogger(RedisController.class);
+
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Autowired
+    private StorageConfig storage;
 
     @GetMapping("/getByKey/{key}")
     public Object getByKey(@PathVariable String key) throws Exception {
@@ -30,6 +47,30 @@ public class RedisController {
     public int deleteByKey(@PathVariable String key) throws Exception {
         this.redisTemplate.opsForValue().getAndDelete(key);
         return 1;
+    }
+
+    @GetMapping("/reduceStorage")
+    public String reduceStorage(@RequestParam(defaultValue = "1") int count) {
+        if (count <= 0) {
+            count = 1;
+        }
+        if (this.storage.getStorage() <= 0) {
+            return "没库存了！";
+        }
+        RLock lock = this.redissonClient.getLock("reduce-storage");
+
+        try {
+            if (lock.tryLock(1, TimeUnit.SECONDS)) {
+                int realCount = this.storage.reduceStorage(count);
+                return "获取到" + realCount + "个库存。";
+            }
+            return "人太多了，请稍后重试！";
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+            lock.unlock();
+        }
+        return "系统繁忙，请稍后重试！";
     }
 
 }
