fe987639ad6cb89568aa0c2db21a2a5efb7d7a7a
王洪亮 <w505957125@aliyun.com>
2024-06-25 14:11:32 +0800

    1. add api-docs demo

--- src/main/java/com/whl/spring/demo/controller/KaptchaController.java
+++ src/main/java/com/whl/spring/demo/controller/KaptchaController.java
@@ -1,6 +1,11 @@
 package com.whl.spring.demo.controller;
 
 import com.google.code.kaptcha.Producer;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.ServletOutputStream;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +22,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 @Controller
 @RequestMapping("/kaptcha")
+@Tag(name = "图片验证码接口", description = "图片验证码接口")
 public class KaptchaController {
     private static final Map<String, String> CACHE = new ConcurrentHashMap<String, String>();
 
@@ -24,6 +30,10 @@ public class KaptchaController {
     private Producer producer;
 
     @GetMapping({"", "/"})
+    @Operation(summary = "图片验证码请求接口", description = "图片验证码请求接口")
+    @Parameters({
+            @Parameter(name = "key", in = ParameterIn.QUERY, required = true, description = "客户端标识，需客户端生成并保存，提交验证码的时候将key也传到后台")
+    })
     public void index(HttpServletResponse response, @RequestParam String key) throws Exception {
         response.setHeader("Cache-Control", "no-store, no-cache");
         response.setContentType("image/jpeg");
@@ -38,6 +48,11 @@ public class KaptchaController {
 
     @GetMapping("/verify")
     @ResponseBody
+    @Operation(summary = "图片验证码验证接口", description = "图片验证码验证接口")
+    @Parameters({
+            @Parameter(name = "key", in = ParameterIn.QUERY, required = true, description = "客户端标识，需客户端生成并保存，提交验证码的时候将key也传到后台"),
+            @Parameter(name = "text", in = ParameterIn.QUERY, required = true, description = "验证码内容")
+    })
     public String verify(@RequestParam String key, @RequestParam String text) {
         String kaptcha = CACHE.get(key);
 
