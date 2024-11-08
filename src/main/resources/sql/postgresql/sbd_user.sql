f245cd4dd8bc1230cb8cf128ed9d3141bdd50261
王洪亮 <w505957125@aliyun.com>
2024-11-08 16:17:24 +0800

    1. add sqlserver/sqlserver/dm support

--- src/main/resources/sql/postgresql/sbd_user.sql
+++ src/main/resources/sql/postgresql/sbd_user.sql
@@ -1,8 +1,10 @@
 CREATE TABLE sbd_user (
-    id serial NOT NULL,
+    id bigint NOT NULL,
     username varchar(255) DEFAULT NULL,
     age int DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE UNIQUE INDEX idx_u_u ON sbd_user (username);
+
+CREATE SEQUENCE seq_user;
